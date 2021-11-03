7056d267567006991a3a6ddad4499277d506a0ca
Malin Malliya Wadu <malin.malliya.wadu@gmail.com>
2021-11-03 15:28:53 +1300

    tweak heaidng

--- frontend/src/components/Heading.tsx
+++ frontend/src/components/Heading.tsx
@@ -1,7 +1,7 @@
 export const Heading: React.FC = ({ children }) => {
   return (
     <h1
-      className="text-5xl md:text-6xl mt-4 mb-4 tracking-tighter text-gray-700 "
+      className="text-5xl md:text-6xl mt-4 mb-4 md:mb-6 tracking-tighter text-gray-700 "
       style={{
         textShadow: "0px 2px 2px rgba(0,0,0,0.2), 0px 3px 4px rgba(0,0,0,0.1)",
       }}
